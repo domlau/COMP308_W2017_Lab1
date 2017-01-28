fe35ebe0481acd599a86d6754bf228e3bf978d88
domlau <dominic.k.y.lau@gmail.com>
2017-01-27 22:39:30 -0500

    Created html pages

---
+++ server.js
@@ -0,0 +1,27 @@
+//The Server runs from here
+//Import the module
+const express = require('express');
+const router = express.Router();
+const app = express();
+const port = 3000;
+
+
+router.get('/' , (req,res) => {
+    res.sendFile(__dirname + '/index.html');
+    
+});
+
+router.get('/about', (req,res) => {
+    res.sendFile(__dirname + '/about.html');
+    
+});
+
+router.get('/contact', (req,res) => {
+    res.sendFile(__dirname + '/contact.html');
+    
+});
+
+app.use('/', router);
+
+app.listen(port);
+console.log(`Server started at http:/localhost:${port}`);
